c8a32265476bce3aa1597dc3f35d39cde30ea47f
oleg-new <9213195@gmail.com>
2023-08-17 14:30:31 +0300

    added "docker-compose.yml" file

--- src/main/java/com/olegnew/jvcboxmanagerv1/JvCboxManagerV1Application.java
+++ src/main/java/com/olegnew/jvcboxmanagerv1/JvCboxManagerV1Application.java
@@ -2,24 +2,11 @@ package com.olegnew.jvcboxmanagerv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class JvCboxManagerV1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(JvCboxManagerV1Application.class, args);
-    }
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().and()
-                .authorizeHttpRequests()
-                
-                .anyRequest().authenticated().and()
-                .build();
     }
 }
--- src/main/java/com/olegnew/jvcboxmanagerv1/config/SecurityConfig.java
+++ src/main/java/com/olegnew/jvcboxmanagerv1/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.olegnew.jvcboxmanagerv1.config;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 import com.olegnew.jvcboxmanagerv1.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +20,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.PATCH;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -56,73 +56,6 @@ public class SecurityConfig {
                 .build();
     }
 
- /*   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("login").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/v1/user/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.POST, "/v1/user/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.PUT, "/v1/user/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.PATCH, "/v1/user/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.DELETE, "/v1/user/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/v1/cbox/**")
-                                .hasAnyRole("OPERATOR", "USER")
-                                .requestMatchers(HttpMethod.PUT, "/v1/cbox/**").hasRole("OPERATOR")
-                                .requestMatchers(HttpMethod.POST, "/v1/cbox/**").hasRole("OPERATOR")
-                                .requestMatchers(HttpMethod.PATCH, "/v1/cbox/**").hasRole("OPERATOR")
-                                .requestMatchers(HttpMethod.DELETE, "/v1/cbox/**").hasRole("OPERATOR")
-                        //.anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
-        return http.build();
-    }
-*/
-/* public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-     http
-             .authorizeHttpRequests((requests) -> requests
-                     .requestMatchers("login").permitAll()
-             .requestMatchers(
-                     "/api/v1/auth/**",
-                     "/v2/api-docs",
-                     "/v3/api-docs",
-                     "/v3/api-docs/**",
-                     "/swagger-resources",
-                     "/swagger-resources/**",
-                     "/configuration/ui",
-                     "/configuration/security",
-                     "/swagger-ui/**",
-                     "/webjars/**",
-                     "/swagger-ui.html"
-             )
-             .permitAll()
-
-
-                     .requestMatchers(GET, "/v1/user/**").hasRole("ADMIN")
-                     .requestMatchers(POST, "/v1/user/**").hasRole("ADMIN")
-                     .requestMatchers(PUT, "/v1/user/**").hasRole("ADMIN")
-                     .requestMatchers(PATCH, "/v1/user/**").hasRole("ADMIN")
-                     .requestMatchers(DELETE, "/v1/user/**").hasRole("ADMIN")
-                     .requestMatchers(GET, "/v1/cbox/**")
-                     .hasAnyRole("OPERATOR", "USER")
-                     .requestMatchers(PUT, "/v1/cbox/**").hasRole("OPERATOR")
-                     .requestMatchers(POST, "/v1/cbox/**").hasRole("OPERATOR")
-                     .requestMatchers(PATCH, "/v1/cbox/**").hasRole("OPERATOR")
-                     .requestMatchers(DELETE, "/v1/cbox/**").hasRole("OPERATOR"))
-             .formLogin((form) -> form
-                     .loginPage("/login")
-                     .permitAll()
-             )
-             .logout((logout) -> logout.permitAll());
-
-
-
-     return http.build();
- }*/
     @Bean
     public PasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder();
